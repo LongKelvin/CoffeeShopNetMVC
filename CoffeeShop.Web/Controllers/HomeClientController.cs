281f2f8420f20ecc2aa6a3553c6f6ee4eb7f389d
Long Kelvin <longkelvin101099@gmail.com>
2022-04-16 17:14:12 +0700

    create client web page

---
+++ CoffeeShop.Web/Controllers/HomeClientController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShop.Web.Controllers
+{
+    [RoutePrefix("Home")]
+    public class HomeClientController : Controller
+    {
+        // GET: HomeClient
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult Footer()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult Category()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult Header()
+        {
+            return PartialView();
+        }
+    }
+}
